905cecac7166a032ad5cfdafcfcfdbffc734f911
SoledadBarcelona <101674297+SoledadBarcelona@users.noreply.github.com>
2022-06-01 07:20:05 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,68 +1,40 @@
-const btn1 = document.getElementById("miBoton1");
-btn1.addEventListener("click", ()=>{
- hola();
- });
+const input2 = document.getElementById("input4");
+const btn = document.getElementById("miBoton2");
+input4.onchange = () => (btn.disabled = false);
 
-function hola()
-{window.location="pages/2.html"};
+btn.addEventListener("click", () => {
+  validarFormulario();
+});
 
+function validarFormulario() {
+  const nombre = document.getElementById("input1");
+  const apellido = document.getElementById("input2");
+  const ciudad = document.getElementById("input3");
+  const provincia = document.getElementById("input4");
 
-
-const input4 = document.getElementById("input4");
- const btn = document.getElementById("miBoton2");
- input4.onchange= ()=> btn.disabled=false;
- 
- btn.addEventListener("click", ()=>{
-     validarFormulario();
-
- });
-
-  btn.addEventListener("mouseup", ()=>{
-     input4.setAttribute("sryle", "display:none") ;
-
- });
-
-
-
- function validarFormulario()
- {
-     const usuario = document.getElementById("input3");
-     const pass = document.getElementById("input4");
-
-           if(usuario.value.trim()!=="" && pass.value.trim()!=="")
-           {
-               alert("Ingrese Uduario y Contraseña")
-                let username = prompt("Ingrese su nombre de usuario");
-                let pwd = prompt("Ingrese su password");
-                if(usuario.value.trim()=="Sergio" && pass.value.trim()=="Duarte" && username=="Sergio.D" && pwd=="Dragon")
-            {
-                alert(`Bienvenido Segio Duarte`);
-                loginExitoso=true; 
-
-            }
-            
-             else if(usuario.value.trim()=="Fernando" && pass.value.trim()=="Duarte" && username=="Vegeta" && pwd=="777")
-            {
-              alert(`Bienvenido Fernando Duarte`);
-              loginExitoso=true; 
-            }
-
-             else if(usuario.value.trim()=="Alexis" && pass.value.trim()=="Isaguirre" && username=="Divididos" && pwd=="Catupecu")
-            {
-              alert(`Bienvenido Segio Duarte`);
-              loginExitoso=true; 
-            }
-
-            else 
-            { 
-              alert("Alguno de los datos ingresados es erroneo")
-             }
-
-     }
-     else
-     {
-         alert("DATOS INVÁLIDOS")
-     }
-
-
- }
+  if (
+    nombre.value.trim() !== "" &&
+    apellido.value.trim() !== "" &&
+    ciudad.value.trim() !== "" &&
+    provincia.value.trim() !== ""
+  ) {
+    alert("Ingrese usuario y password");
+    let username = prompt("Ingrese su usuario ");
+    let pwd = prompt("Ingrese su password");
+    if (
+      nombre.value.trim() == "Soledad" &&
+      apellido.value.trim() == "Barcelona" &&
+      username == "s.barcelona" &&
+      pwd == "java"
+    ) {
+      alert(
+        `Bienvenido/a ${nombre.value.trim()} ${apellido.value.trim()} de la ciudad de ${ciudad.value.trim()}, provincia de ${provincia.value.trim()}  `
+      );
+      loginExitoso = true;
+    } else {
+      alert("Ha ingresado datos erroneos");
+    }
+  } else {
+    alert("Vuelva a ingresar");
+  }
+}
